c5f3d878fd99e685913e3041fc85d24b92f399e5
Ian <zeltkimo+github@gmail.com>
2020-03-13 17:28:09 +0800

    note

--- app/src/main/java/tw/org/iii/brad/brad03/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad/brad03/MainActivity.java
@@ -1,5 +1,5 @@
 package tw.org.iii.brad.brad03;
-
+//ListView, Toast, SimpleAdapter
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView lv;
     private SimpleAdapter simpleAdapter;
     private ArrayList<HashMap<String,String>> data = new ArrayList<>();
-    private String[] from = {"field1","field2","field3"};
+    private String[] from = {"field1","field2","field3"}; //此處代表的是key的值,與value無關,故可隨意取名
     //用key抓到value, from指要用的value, to是丟到哪個地方呈現
     private int[] to = {R.id.itemname_title,R.id.itemname_content,R.id.itemname_date};
     //用id指向
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             data.add(row0);
         }
 
-        simpleAdapter = new SimpleAdapter(this,data,R.layout.itemname,from,to);
+        simpleAdapter = new SimpleAdapter(this,data,R.layout.itemname,from,to);//5個參數,2為資料(需為List,內為Map的第一參數是String),3為要呈現的地方(右鍵專案new一個layout)
         lv.setAdapter(simpleAdapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -47,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 showOneItem(position);
             }
-        });
+        });//listview裡面的項目被點到時OnItemClick
     }
 
     private void showOneItem(int pos){
-        Toast.makeText(this,data.get(pos).get(from[0]),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,data.get(pos).get(from[0]),Toast.LENGTH_SHORT).show();//記得show
     }
 
     public void addItem(View view) {
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         row0.put(from[0],"value"+(int)(Math.random()*100 + 100));
         row0.put(from[1],"new data");
         row0.put(from[2],"2050-01-01 12:00:00");
-        data.add(0,row0);
+        data.add(0,row0);//將新增資料加在起始處
         simpleAdapter.notifyDataSetChanged();
     }
 }
